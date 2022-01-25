2650bebb4e12d1a420feb0332c077b0259f999a0
Un-Original1 <78579980+Un-Original1@users.noreply.github.com>
2022-01-25 13:14:30 -0500

    Delete ancientbeasts directory

--- ancientbeasts/potions/PotionPossessed.java
+++
@@ -1,9 +0,0 @@
-package com.unoriginal.ancientbeasts.potions;
-
-public class PotionPossessed extends PotionBase{
-    public PotionPossessed(boolean isBadEffectIn, int liquidColorIn) {
-        super(isBadEffectIn, liquidColorIn);
-        setPotionName("potion.possessed");
-        setIconIndex(1,0);
-    }
-}
